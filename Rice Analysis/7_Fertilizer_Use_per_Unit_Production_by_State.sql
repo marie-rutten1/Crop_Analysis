46723b8da37367bbe54f9d418613c744a1e41166
marie-rutten1 <marie.k.rutten@gmail.com>
2025-06-23 19:19:24 +1200

    Update 7_Fertilizer_Use_per_Unit_Production_by_State.sql

--- Rice Analysis/7_Fertilizer_Use_per_Unit_Production_by_State.sql
+++ Rice Analysis/7_Fertilizer_Use_per_Unit_Production_by_State.sql
@@ -1,7 +1,7 @@
 SELECT	
 	State,
 	AVG(Fertilizer/Production) AS Fertilizer_per_Production
-	FROM Crop_Yield
+FROM Crop_Yield
 GROUP BY Crop, State
 HAVING CROP= 'Rice'
 ORDER BY Fertilizer_per_Production;
