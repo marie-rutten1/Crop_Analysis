7111162f0ca295e647f2c974caea4ea629a33283
marie-rutten1 <marie.k.rutten@gmail.com>
2025-06-23 15:04:59 +1200

    Update 17_Comparrison_Rice_Inputs_Output_Punjab_Tamil_Nadu.sql

--- Rice Analysis/17_Comparrison_Rice_Inputs_Output_Punjab_Tamil_Nadu.sql
+++ Rice Analysis/17_Comparrison_Rice_Inputs_Output_Punjab_Tamil_Nadu.sql
@@ -5,10 +5,10 @@ SELECT
   AVG(Pesticide / Area) AS avg_pesticide_per_ha,
   AVG(Production / Fertilizer) AS production_per_unit_fertilizer,
   AVG(Production / Pesticide) AS production_per_unit_pesticide,
-	AVG(Annual_Rainfall) AS avg_annual_rainfall,
-	AVG(Area) AS avg_area,
-	AVG(Production) AS avg_production,
-	SUM(Production) AS total_production
+  AVG(Annual_Rainfall) AS avg_annual_rainfall,
+  AVG(Area) AS avg_area,
+  AVG(Production) AS avg_production,
+  SUM(Production) AS total_production
 FROM Crop_Yield
 WHERE Crop = 'Rice'
   AND State IN ('Punjab', 'Tamil Nadu')
